07f3f4ccd5f8027714b66955436d48f48aea2088
Ahmed Osama <16968919+ahmedusamaali@users.noreply.github.com>
2021-08-15 22:14:12 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+auther Ahmed Osama
+
+
+first : install npm
+npm install
+next : 
+
+npm run build-prod to Build project
+or npm run build-dev to Build project development
+then
+npm start  to project
---
+++ src/client/index.js
@@ -0,0 +1,17 @@
+// js files
+import { checkForURL } from './js/urlChecker'
+import { handleSubmit } from './js/formHandler'
+
+// console.log(checkForURL);
+
+// alert("I EXIST")
+// console.log("CHANGE!!");
+
+// sass files
+import './styles/style.scss'
+
+
+export {
+    checkForURL,
+    handleSubmit
+   }
